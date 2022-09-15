efe095256285bc2b31480a6b9d583155df094f25
Honza Charamza <honza@charamza.cz>
2022-09-16 01:11:27 +0200

    ws server

--- game/src/main.ts
+++ game/src/main.ts
@@ -1,6 +1,13 @@
 async function main() {
   const greetWho = import.meta.env.VITE_GREET;
   console.log(`Hello, ${greetWho} ;)`);
+
+  const ws = new WebSocket(`ws://${import.meta.env.VITE_BACKEND_HOST}`);
+  ws.onmessage = async (event) => console.log("[RECV]", await event.data.text());
+  ws.onopen = () => {
+    console.log("Connected");
+    ws.send("Hello from the browser!");
+  };
 }
 
 main();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { WebSocketServer } from "ws";
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+const wsServer = new WebSocketServer({ noServer: true });
+wsServer.on("connection", (socket) => {
+  socket.on("message", (message) => socket.send(message));
+});
+
+app.use(cors("*"));
 
 app.get(["/", "/:name"], (req, res) => {
   const greeting = "Hello From Node on Fly!!!";
@@ -9,4 +18,9 @@ app.get(["/", "/:name"], (req, res) => {
   res.send(greeting + (name ? ` and hello to ${name}` : ""));
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+const httpServer = app.listen(port, () => console.log(`Server listening on port ${port}!`));
+httpServer.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
