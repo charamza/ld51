f42a35f6d2b2f065f59173f9dda26dc73e7399b7
Honza Charamza <honza@charamza.cz>
2022-09-16 01:16:04 +0200

    wss fix

--- game/src/main.ts
+++ game/src/main.ts
@@ -2,7 +2,8 @@ async function main() {
   const greetWho = import.meta.env.VITE_GREET;
   console.log(`Hello, ${greetWho} ;)`);
 
-  const ws = new WebSocket(`ws://${import.meta.env.VITE_BACKEND_HOST}`);
+  const secure = import.meta.env.VITE_BACKEND_SECURE === "true";
+  const ws = new WebSocket(`${secure ? "wss" : "ws"}://${import.meta.env.VITE_BACKEND_HOST}`);
   ws.onmessage = async (event) => console.log("[RECV]", await event.data.text());
   ws.onopen = () => {
     console.log("Connected");
